20846b2a2ecf46c8d7aefd092f8bf77ac212bb39
Tristram Woolf <tristram@woolfsystems.com>
2022-12-13 16:38:32 +0000

    chore: cleanup

--- tests/frontend.spec.ts
+++ tests/frontend.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test.describe('Frontend', () => {
   test('has title', async ({ page }) => {
+    await page.goto('/')
     const title = page.locator('main > div > p.text-elem')
     await expect(title).toBeVisible()
     await expect(title).toContainText(['Boilerplate Project'])
